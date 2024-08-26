684f8421f9dbebd073b7e2bbf8b9afbcac35f053
AndyC5H10O5 <1915965336@qq.com>
2024-08-26 12:15:48 +0800

    学习了全栈课程P13，应用了

--- src/components/Vuextest.vue
+++ src/components/Vuextest.vue
@@ -1,21 +1,32 @@
 <template>
+  <!-- 获取Vuex的全局共享值的两种方法 -->
   <h2>{{ count }}</h2>
   <h2>{{ this.$store.state.count }}</h2>
   <button @click="add">+1</button>
+
+  <ul>
+    <li v-for="todo in doneTodos" :key="todo.id">{{todo.text}}</li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: "Vuex测试",
-  // Vuex核心概念：State计算属性, 省略this.$store.state
+  
   computed: {
+    // Vuex核心功能：State计算属性, 省略this.$store.state，通过count直接取值
     count() {
-      return store.state.count;
+      return this.$store.state.count;
     },
+
+    // Vuex功能：Getters计算属性，省略this.$store.getters，通过doneTodos直接取值
+    doneTodos(){
+      return this.$store.getters.doneTodos;
+    }
   },
   methods: {
     add() {
-      this.$store.commit("increment");
+      this.$store.commit("increment", 2);
     },
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,28 @@ import { createStore } from 'vuex'
 
 // 创建一个新的 store 实例
 const store = createStore({
+  // 使用Vuex的State功能（基础功能）
   state () {
     return {
-      count: 0
+      count: 0,
+      todos: [
+        { id: 1, text: '投简历', done: true },
+        { id: 2, text: '面试', done: false }
+      ]
     }
   },
+
+  // 使用Vuex的Getters功能（可以过滤）
+  getters: {
+    doneTodos (state) {
+      return state.todos.filter(todo => todo.done)
+    }
+  },
+
+  // 使用Vuex的Mutations功能（可以传参）
   mutations: {
-    increment (state) {
-      state.count++
+    increment (state, n) {
+      state.count += n
     }
   }
 })
