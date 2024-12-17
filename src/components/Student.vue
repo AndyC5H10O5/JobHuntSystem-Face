3220eb47d7121fea6efe8fc8f3feff61356ba2c0
AndyC5H10O5 <1915965336@qq.com>
2024-12-18 01:55:26 +0800

    在表格中的每一行加入了按钮，实现了点击Delete按钮后，删除该行数据的功能

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -5,6 +5,20 @@
     <el-table-column prop="name" label="姓名" width="180" />
     <el-table-column prop="major" label="专业" width="180" />
     <el-table-column prop="gpa" label="绩点" />
+    <el-table-column align="right">
+      <template #default="scope">
+        <el-button size="small" @click="handleEdit(scope.$index, scope.row)">
+          Edit
+        </el-button>
+        <el-button
+          size="small"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)"
+        >
+          Delete
+        </el-button>
+      </template>
+    </el-table-column>
   </el-table>
 
   <label for="a">学号:</label>
@@ -47,8 +61,20 @@ export default {
         });
       });
     },
+    handleDelete(index, row) {
+      // 向服务器发送DELETE请求
+      axios.delete(`/stu/deleteStudent?stuID=${row.stuID}`).then((response) => {
+        console.log(response.data);
+        // 删除成功后，更新表格数据
+        axios.get("/stu/findAllStudents").then((response) => {
+          this.tableData = response.data; // 更新表格数据
+        });
+      }).catch((error) => {
+        console.error("删除失败:", error);
+      });
+    },
   },
-  data: function () {
+  data() {
     return {
       tableData: [],
       stuID: "",
